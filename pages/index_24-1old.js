35036bde13f80774c8e332f77593ff9973f445b4
Niko Cremer <n.ko@posteo.de>
2024-01-24 20:51:21 +0100

    archiv file for old index.js

---
+++ pages/index_24-1old.js
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import Loading from "@/components/Loading/Loading.styled";
+import ErrorMessage from "@/components/Error/Error.styled";
+// import { useRouter } from "next/router";
+import useSWR from "swr";
+import { uid } from "uid";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function HomePage() {
+  const URL1 = `https://swapi.dev/api/people/`;
+  const { data: characters, error, isLoading } = useSWR(URL1, fetcher);
+  console.log("index", characters);
+  if (isLoading) return <Loading />;
+  if (error) return <ErrorMessage />;
+
+  return (
+    <Layout>
+      <h1>React Data Fetching: Star Wars</h1>
+      <List>
+        <li>
+          <StyledLink href="/characters/12">12 Links</StyledLink>
+        </li>
+        {characters.results.map((character, index) => {
+          return (
+            <li key={uid}>
+              <StyledLink href={{ path: `characters/${index + 1}` }}>
+                {character.name}
+              </StyledLink>
+            </li>
+          );
+        })}
+        <li>
+          <StyledLink href="/characters/1">Luke Skywalker</StyledLink>
+        </li>
+        <li>
+          <StyledLink href="/characters/2">C-3PO</StyledLink>
+        </li>
+        <li>
+          <StyledLink href="/characters/3">R2-D2</StyledLink>
+        </li>
+        <li>
+          <StyledLink href="/characters/4">Darth Vader</StyledLink>
+        </li>
+      </List>
+    </Layout>
+  );
+}
+
+const List = styled.ul`
+  background-color: black;
+  list-style-type: "✨  ";
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding: 1rem 2rem;
+  border-radius: 5px;
+  text-decoration: none;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: var(--color-warm);
+  &&:hover {
+    border: 1px solid var(--color-warm);
+    padding: 0.4rem;
+    border-radius: 12px;
+    color: #e9c62d;
+  }
+`;
+
+/*
+<List>
+        <li>
+          <StyledLink href="/characters/1">Luke Skywalker</StyledLink>
+        </li>
+        <li>
+          <StyledLink href="/characters/2">C-3PO</StyledLink>
+        </li>
+        <li>
+          <StyledLink href="/characters/3">R2-D2</StyledLink>
+        </li>
+        <li>
+          <StyledLink href="/characters/4">Darth Vader</StyledLink>
+        </li>
+      </List>
+*/
